068cf32bd7cf6c8fdf3cd6dd3c5da4ccf2cd3148
Anupam <97234085+AnupamSwami@users.noreply.github.com>
2023-10-24 18:51:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function add_user () {
+    window.location = "game.html"
+    var player1_name = document.getElementById("player1_input").value;
+
+    var player2_name = document.getElementById("player2_input").value;
+
+    localStorage.setItem("Player 1", player1_name);
+
+    localStorage.setItem("Player 2", player2_name);
+}
